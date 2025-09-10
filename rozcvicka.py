e9838b8e32eaa6318f9a70a1a8373643d536b182
Jan Klanica <jklanica@email.cz>
2025-09-10 15:58:49 +0200

    feat: ahoj comment

--- rozcvicka.py
+++ rozcvicka.py
@@ -2,10 +2,11 @@ user_input = input("Zadejte text: ")
 
 for letter in user_input:
     print(letter)
-    
+
 print(f"Letter has {len(user_input)} marks.")
 
 # Nech uživatele něco napsat a poté vzpiš co napsal pod sebe.
 # Poté spočítej všechny proměnné a součet napiš do věty.
-------
+
+# ahoj
 
